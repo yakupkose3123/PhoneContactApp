c8f2e7a92cde88f09df1b30abdf3c5c9269f37ea
YAKUP KOSE <yakupkose3123@gmail.com>
2022-08-01 17:52:39 +0300

    first commit

---
+++ server/contact/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from .serializers import ContactSerializer
+from .models import Contact
+
+class ContactList(generics.ListCreateAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
+class ContactRUD(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
